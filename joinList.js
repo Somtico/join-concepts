d7025896492e810103b93865b66a52caaddf1445
Somto Ufondu <somtoufondu@gmail.com>
2024-03-24 18:42:10 -0600

    Completed the challenge.

--- joinList.js
+++ joinList.js
@@ -3,16 +3,32 @@
  * into one String, concepts, with each list item separated from
  * the previous by a comma.
  *
- * To implement this we'll create a joinList function which will take 
+ * To implement this we'll create a joinList function which will take
  * in any array of strings return a comma-separated string.
  *
  * Note: We can NOT use the built-in Array join function.
  */
 
 // Write our function (we must define it too!) below
-// ...
+const joinList = function(conceptList) {
+  // Initialize a variable to store the items from the array in the joinList argument
+  let newString = "";
+  // Loop over argument
+  for (let i = 0; i < conceptList.length; i++) {
+    // Concatenate the items from the array and add them to the newString variable
+    newString += conceptList[i];
+    // Check to be sure it is not the last item then add a comma to separate them
+    if (i < conceptList.length - 1) {
+      newString += ", ";
+    }
+  }
+  return newString;
+};
 
 // Test / Driver Code below...
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
 const concepts = joinList(conceptList);
 console.log(`Today I learned about ${concepts}.`);
+
+// Test with an empty array
+console.log(joinList([]));
